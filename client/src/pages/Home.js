7c664b76539b69e0dc3511ed902a118afabaab56
vayne <miguel.osorio19@yahoo.com>
2022-04-05 14:21:52 -0500

    setting up reducers

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -14,3 +14,7 @@ const Home = () => {
 };
 
 export default Home;
+
+// Home.js manages the state that's updated by the CategoryMenu component and used by the ProductList component
+// The Home Page component manages teh state currentCategory, which is passed to the ProductList component as a prop and instructs which category's products should be retrieved using Apollo.
+// To set that currentCategory value, however, the setCategory callback function is passed to the CategoryMenu component as a prop to be executed on a new category pick
