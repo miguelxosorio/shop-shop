2e6e1e07089059a3c2c2b239839147646c15968d
vayne <miguel.osorio19@yahoo.com>
2022-04-07 16:25:33 -0500

    updated cart()

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -2,11 +2,35 @@ import React from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
+import { useStoreContext } from '../../utils/GlobalState';
+import { TOGGLE_CART } from '../../utils/actions';
 
 const Cart = () => {
+  const [state, dispatch] = useStoreContext();
+
+  // dispatch() will call the TOGGLE_CART action
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
+  // If cartOpen is false, the component will return a much smaller <div>
+  // Clicking this <div> will set cartOpen to true and return the expanded shopping cart
+  if (!state.cartOpen) {
+    return (
+      <div className="cart-closed" onClick={toggleCart}>
+        <span role="img" aria-label="trash">
+          🛒
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="cart">
-      <div className="close">[close]</div>
+      {/* handler will toggle the cartOpen value whenever the [close] text is clicked */}
+      <div className="close" onClick={toggleCart}>
+        [close]
+      </div>
       <h2>Shopping Cart</h2>
       <div>
         <CartItem
