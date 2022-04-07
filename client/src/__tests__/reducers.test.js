2ca698dbfbea42e84124a43236ebbf66fe396c7f
vayne <miguel.osorio19@yahoo.com>
2022-04-07 11:57:45 -0500

    added actions to perform on CART functionality

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -2,7 +2,13 @@
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from '../utils/actions';
 
 // import the reducer() function
