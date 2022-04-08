cae1c74e9c87aeb9f6ebd68d57b0a8793b9ba371
vayne <miguel.osorio19@yahoo.com>
2022-04-08 13:57:35 -0500

    updated cart function, added onChange event

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -5,7 +5,7 @@ import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 // This component currently gets all of its data as props passed down from Cart
 // CartItem will also update the global state to adjust item quantities
 const CartItem = ({ item }) => {
-// we only destructured the dispatch() function from the useStoreContext Hook, because the CartItem component has no need to read state
+  // we only destructured the dispatch() function from the useStoreContext Hook, because the CartItem component has no need to read state
   const [, dispatch] = useStoreContext();
 
   const removeFromCart = item => {
@@ -13,6 +13,27 @@ const CartItem = ({ item }) => {
       type: REMOVE_FROM_CART,
       _id: item._id,
     });
+  };
+
+  // Anytime an <input> element's value changes, an onChange event will occur
+  // capture that event and send the element's new value to the reducer
+  const onChange = e => {
+    const value = e.target.value;
+
+    // if the user changes the quantity to zero, we should simply delete the item from the cart with the REMOVE_FROM_CART action
+    if (value === '0') {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    // Otherwise, we'll call the UPDATE_CART_QUANTITY action
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
   };
 
   return (
@@ -26,8 +47,12 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} />
-          <span role="img" aria-label="trash" onClick={() => removeFromCart(item)}>
+          <input type="number" placeholder="1" value={item.purchaseQuantity} onChange={onChange} />
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
             🗑️
           </span>
         </div>
